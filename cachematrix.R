72403fd568e575658dea8e2fe1b5797fa271b275
Merve Melis Mirza <melismirza@gmail.com>
2018-05-17 21:35:07 +0300

    Programming Assignment Week 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  get <- function() x
+  set <- function(mNew){
+    x <<- mNew
+    inv <<- NULL
+  }
+  setInv <- function(inved) inv <<- inved
+  getInv <- function() inv
+  list(get = get,set = set, setInv = setInv, getInv = getInv)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  dat <- x$get()
+  inv <- solve(dat,...)
+  x$setInv(inv)
+  inv
 }
